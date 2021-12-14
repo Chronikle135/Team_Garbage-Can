0208a8ee348949ddf97e95121148afac9ea5997e
Chronikle <Simon.Nenning@gmx.at>
2021-12-14 11:52:14 +0100

    First timer tryout

---
+++ src/main/java/at/acfhcampus/stud/team_garbagecan/Upgrades.java
@@ -0,0 +1,11 @@
+package at.acfhcampus.stud.team_garbagecan;
+import java.util.TimerTask;
+
+class Timerino {
+    TimerTask cond = new TimerTask() {
+        @Override
+        public void run() {
+            //hier die Updates rein.
+        }
+    };
+}
---
+++ src/main/java/at/acfhcampus/stud/team_garbagecan/canclicker.java
@@ -0,0 +1,16 @@
+package at.acfhcampus.stud.team_garbagecan;
+import Upgrades.java;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class canclicker {
+    public static void main(String[] args) {
+    Timer tick = new Timer();
+    tick.schedule(cond, );
+
+
+
+
+    }
+}
+
